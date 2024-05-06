de2ec3fa3442aaade668fcca3d9bbdd439feb9fd
yash ahire <yashahahire1234@gmail.com>
2024-05-06 14:19:55 +0530

    all set

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(`mongodb://127.0.0.1:27017/habits_db`);
-
+mongoose.connect(process.env.MONGO_URI)
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'Error connecting to the db'));
--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 const expressLayouts= require('express-ejs-layouts')
-
+const dotenv = require('dotenv');
+dotenv.config();
 const port=9000;
 app.use(express.urlencoded({ extended: true }))
 
